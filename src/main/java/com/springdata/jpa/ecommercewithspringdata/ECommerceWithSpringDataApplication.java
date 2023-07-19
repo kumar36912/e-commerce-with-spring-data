738bbf2a35b45b1bdf4cef834a566a3d90c8e661
Senthil Kumar P <kumar36912@gmail.com>
2023-07-19 19:43:09 +0530

    Initial commit

---
+++ src/main/java/com/springdata/jpa/ecommercewithspringdata/ECommerceWithSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.springdata.jpa.ecommercewithspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceWithSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceWithSpringDataApplication.class, args);
+	}
+
+}
